4a0a3b5fe8bf937abe5892e71174e217f4640c21
Pranav <mohnapranav@gmail.com>
2025-12-24 20:11:38 +0530

    Update API_URL to use production endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+const API_URL = process.env.REACT_APP_API_URL || "http:ats-resume-scanner-upgrade-version.onrender.com";
 
 export const analyzeResumes = async (formData) => {
   try {
